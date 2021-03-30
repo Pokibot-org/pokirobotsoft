7679c5ac627737ff0a61e43b47443823f60f0af5
bcluzel <baptiste.clu@gmail.com>
2021-03-31 00:27:28 +0200

    Updated main speed calculation

--- src/main.c
+++ src/main.c
@@ -4,23 +4,21 @@
 
 LOG_MODULE_REGISTER(main_task);
 
-uint32_t start;
+uint32_t time_start;
 void tmp_clbk(const path_node_t * node, void * ucfg){
-    uint32_t end = k_cycle_get_32();
-
-    uint32_t diff = (float)(end-start) * 1000/sys_clock_hw_cycles_per_sec();
-    LOG_INF("Finding path took %d msec", diff);
+    uint32_t diff =  k_uptime_get_32() - time_start;
+    LOG_INF("Finding path took %u msec", diff);
 }
-
 
 int main(void)
 {
 
     LOG_INF("Main Booted\n");
-
+ 
     LOG_INF("Launching pathfinding in 1s\n");
     k_sleep(K_SECONDS(1));
-    start = k_cycle_get_32();
+    time_start = k_uptime_get_32();
+
     path_manager_config_t cfg = {
         .found_path_clbk = tmp_clbk,
         .found_updated_path_clbk = NULL,
