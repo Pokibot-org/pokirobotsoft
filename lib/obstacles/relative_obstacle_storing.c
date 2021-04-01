cfdd50cada1b081b0396763d154c31901374b401
bcluzel <baptiste.clu@gmail.com>
2021-04-01 18:53:05 +0200

    Use float and not double constants

--- lib/obstacles/relative_obstacle_storing.c
+++ lib/obstacles/relative_obstacle_storing.c
@@ -3,9 +3,9 @@
 #else
 #include <math.h>
 #include <zephyr.h>
-#define M_PI		3.14159265358979323846
-#define M_PI_2		1.57079632679489661923
-#define M_PI_4		0.78539816339744830962
+#define M_PI		3.14159265358979323846f
+#define M_PI_2		1.57079632679489661923f
+#define M_PI_4		0.78539816339744830962f
 #endif
 #include "relative_obstacle_storing.h"
 
