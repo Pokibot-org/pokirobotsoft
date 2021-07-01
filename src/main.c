990a6ce3db860eac466e8c40c40cb8080160ca57
Mathieu PILLET <math.pillet@hotmail.com>
2021-07-01 22:16:32 +0200

    WIP caca

--- src/main.c
+++ src/main.c
@@ -1,43 +1,42 @@
 #include <zephyr.h>
 #include <logging/log.h>
-#include "path_manager.h"
-#include "speedgen.h"
+#include <drivers/spi.h>
+
+#include "as5047p.h"
 
 LOG_MODULE_REGISTER(main_task);
-
-uint32_t time_start;
-void tmp_clbk(const path_node_t * node, void * ucfg){
-    uint32_t diff =  k_uptime_get_32() - time_start;
-    LOG_INF("Finding path took %u msec", diff);
-}
 
 int main(void)
 {
 
-    LOG_INF("Main Booted\n");
- 
-    LOG_INF("Launching pathfinding in 1s\n");
-    k_sleep(K_SECONDS(1));
-    time_start = k_uptime_get_32();
+    LOG_INF("boot\n");
 
-    path_manager_config_t cfg = {
-        .found_path_clbk = tmp_clbk,
-        .found_updated_path_clbk = NULL,
-        .user_config = NULL
-    };
-    coordinates_t path_start = {
-        .x = 10,
-        .y = 10,
-    };
-    coordinates_t path_end = {
-        .x = 2990,
-        .y = 10,
-    };
+    const struct device* spi = device_get_binding(DT_LABEL(DT_ALIAS(enc_left)));
+    if (!spi) {
+        LOG_ERR("failed to get SPI");
+        while (1)
+        {
+            k_sleep(K_MSEC(1000));
+            LOG_INF("fail device_get\n");
+        }
+    }
 
-    path_manager_find_path(path_start, path_end, cfg);
+    as5047p enc_left;
+    as5047p_init(&enc_left, spi);
+
     while (1)
     {
         k_sleep(K_MSEC(1000));
+        uint32_t val;
+        //int ret = 0;
+        int ret = as5047p_read(&enc_left, &val);
+        if (!ret) {
+            LOG_INF("success read\n");
+        } else {
+            LOG_INF("fail read\n");
+        }
     }
+
+    return 0;
 
 }
