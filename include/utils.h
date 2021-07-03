988d73e70b668238715799edc47cee1fdbcc2dba
Mathieu PILLET <math.pillet@hotmail.com>
2021-07-04 01:10:37 +0200

    rework as5047p but it's awful

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,9 @@
+
 #ifndef PATHFINDING_UTILS_H
 #define PATHFINDING_UTILS_H
+
 #include "common_types.h"
+
 
 #define SQUARE(a) ((a) * (a))
 #define ABS(a) ((a) < 0 ? -(a) : (a))
@@ -15,3 +18,4 @@ void utils_init_rand_seed(uint32_t tab[4]);
 uint32_t utils_max(uint32_t a, uint32_t b);
 
 #endif
+
--- lib/encoder/as5047p.h
+++ lib/encoder/as5047p.h
@@ -4,6 +4,29 @@
 #include <zephyr.h>
 #include <drivers/spi.h>
 
+#define SPI_AS5047P_FREQ        500000U
+#define SPI_AS5047P_OP          (SPI_OP_MODE_MASTER | SPI_MODE_CPHA | SPI_TRANSFER_MSB | SPI_WORD_SET(16))
+#define SPI_AS5047P_CS_FLAGS    GPIO_ACTIVE_LOW
+#define SPI_AS5047P_CS_DELAY    0
+
+
+#define SPI_AS5047P_CONFIG(_slave, _cs_ctrl)                                    \
+    (struct spi_config) {                                                       \
+        .frequency = SPI_AS5047P_FREQ,                                          \
+        .operation = SPI_AS5047P_OP,                                            \
+        .slave = (_slave),                                                      \
+        .cs = (_cs_ctrl)                                                        \
+    }
+
+#define SPI_AS5047P_CS_CTRL(_gpio_dev, _pin)                                    \
+    (struct spi_cs_control) {                                                   \
+        .gpio_dev = (_gpio_dev),                                                \
+        .delay = SPI_AS5047P_CS_DELAY,                                          \
+        .gpio_pin = (_pin),                                                     \
+        .gpio_dt_flags = SPI_AS5047P_CS_FLAGS                                   \
+    }
+
+
 typedef struct as5047p
 {
     const struct device* spi;
@@ -11,7 +34,9 @@ typedef struct as5047p
     struct spi_cs_control cs_ctrl;
 } as5047p;
 
-int as5047p_init(as5047p* encoder, const struct device* spi);
+
+int as5047p_init(struct as5047p* dev, const struct device* spi, uint16_t slave,
+        const struct device* gpio_dev, gpio_pin_t pin);
 int as5047p_read(const as5047p* encoder, uint16_t* val);
 
 #endif // AS5047P_H
