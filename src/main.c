da675ac3ef8d06eec356d0887e1620471a9e0fd1
Mathieu PILLET <math.pillet@hotmail.com>
2021-07-04 23:54:52 +0200

    clean as5047p a bit

--- src/main.c
+++ src/main.c
@@ -6,41 +6,41 @@
 
 #include "as5047p.h"
 
-LOG_MODULE_REGISTER(main_task);
+LOG_MODULE_REGISTER(main);
 
 
-int main(void)
-{
+int main(void) {
 
     LOG_INF("boot\n");
 
-    const struct device* spi = device_get_binding(DT_LABEL(DT_ALIAS(enc_left)));
+    const struct device* spi = device_get_binding(
+            DT_LABEL(DT_ALIAS(spi_as5047p)));
     if (!spi) {
         LOG_ERR("failed to get SPI");
-        while (1)
-        {
+        while (1) {
             k_sleep(K_MSEC(1000));
             LOG_INF("fail device_get");
         }
     }
 
     as5047p enc_l;
+    as5047p enc_r;
     as5047p_init(&enc_l, spi, 0, DEVICE_DT_GET(DT_NODELABEL(gpioa)), 4);
-    //as5047p_init(&enc_l, spi);
+    as5047p_init(&enc_r, spi, 0, DEVICE_DT_GET(DT_NODELABEL(gpioa)), 15);
 
-
-    //int a = DT_SPI_DEV_HAS_CS_GPIOS(DT_NODELABEL(as5047p_r));
-    //DT_NODELABEL(as5047p_r);
-    //struct spi_cs_control *cs_ctrl = SPI_CS_CONTROL_PTR_DT(DT_NODELABEL(as5047p_r), 0);
-    //LOG_INF("%d", a);
-
-    while (1)
-    {
+    while (1) {
+        uint16_t val_l, val_r;
         k_sleep(K_MSEC(1000));
-        uint16_t val = 1;
-        //int ret = 0;
-        int ret = as5047p_read(&enc_l, &val);
-        LOG_INF("transceive (ret=%d): par=%x err=%x  ---  %d", ret, (val & (1<<15)) >> 15, (val & (1<<14)) >> 14, val & 0x3fff);
+        int ret_l = as5047p_read(&enc_l, &val_l);
+        int ret_r = as5047p_read(&enc_r, &val_r);
+        LOG_INF("read_r (ret=%d): par=%x err=%x  ---  %d",
+                ret_l,
+                (val_l & (1<<15)) >> 15,
+                (val_l & (1<<14)) >> 14, val_l & 0x3fff);
+        LOG_INF("read_r (ret=%d): par=%x err=%x  ---  %d",
+                ret_r,
+                (val_r & (1<<15)) >> 15,
+                (val_r & (1<<14)) >> 14, val_r & 0x3fff);
     }
 
     return 0;
