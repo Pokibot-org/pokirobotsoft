163e0e877adf1e5f6315efb0dd43c75d27e38620
Mathieu PILLET <math.pillet@hotmail.com>
2021-07-04 23:54:52 +0200

    clean as5047p a bit

--- include/config.h
+++ include/config.h
@@ -2,8 +2,5 @@
 #include <devicetree.h>
 #include <drivers/spi.h>
 
-// spi for as5047p magnetic encoders
-#define SPI_AS5047P_FREQ        500000U
-#define SPI_AS5047P_OP          (SPI_OP_MODE_MASTER | SPI_MODE_CPHA | SPI_TRANSFER_MSB | SPI_WORD_SET(16))
-#define SPI_AS5047P_CS_FLAGS    GPIO_ACTIVE_LOW
+
 
--- lib/as5047p/as5047p.h
+++ lib/as5047p/as5047p.h
@@ -3,9 +3,11 @@
 
 #include <zephyr.h>
 #include <drivers/spi.h>
+#include <drivers/gpio.h>
 
 #define SPI_AS5047P_FREQ        500000U
-#define SPI_AS5047P_OP          (SPI_OP_MODE_MASTER | SPI_MODE_CPHA | SPI_TRANSFER_MSB | SPI_WORD_SET(16))
+#define SPI_AS5047P_OP          (SPI_OP_MODE_MASTER | SPI_MODE_CPHA | \
+                                 SPI_TRANSFER_MSB | SPI_WORD_SET(16))
 #define SPI_AS5047P_CS_FLAGS    GPIO_ACTIVE_LOW
 #define SPI_AS5047P_CS_DELAY    0
 
@@ -37,6 +39,7 @@ typedef struct as5047p
 
 int as5047p_init(struct as5047p* dev, const struct device* spi, uint16_t slave,
         const struct device* gpio_dev, gpio_pin_t pin);
-int as5047p_read(const as5047p* encoder, uint16_t* val);
+int as5047p_read(const as5047p* encoder, uint16_t* rval);
+int as5047p_transeive(const as5047p* encoder, uint16_t tval, uint16_t* rval);
 
 #endif // AS5047P_H
