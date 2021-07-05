4d9e2926050e2fe3a1f6429055c7a18098ddf4a4
bcluzel <baptiste.clu@gmail.com>
2021-07-05 13:20:10 +0200

    utils now use float

--- src/utils.c
+++ src/utils.c
@@ -8,57 +8,36 @@
 #include <random/rand32.h>
 #endif
 
-// TODO
-// Remove one liner functions by macros
-// Or inline functions when type safety is a must
-
-/*
-if opti needed
-double fsqrt (double y) {
-    double x, z, tempf;
-    unsigned long *tfptr = ((unsigned long *)&tempf) + 1;
-
-	tempf = y;
-	*tfptr = (0xbfcdd90a - *tfptr)>>1;
-	x =  tempf;
-	z =  y*0.5;                        
-	x = (1.5*x) - (x*x)*(x*z);         
-	x = (1.5*x) – (x*x)*(x*z);
-	x = (1.5*x) – (x*x)*(x*z);
-	x = (1.5*x) – (x*x)*(x*z);
-	x = (1.5*x) – (x*x)*(x*z);
-	return x*y;
-    }
-*/
+#define MAX(a, b) ((a)>(b) ? (a) : (b))
 
 // TODO: Octile distance h(x) = max( (x1 – x2), (y1 – y2) + (sqrt(2) -1) * min( (x1 – x2), (y1 – y2))
 
-uint32_t utils_distance(const coordinates_t *a, const coordinates_t *b)
+inline float utils_distance(const coordinates_t *a, const coordinates_t *b)
 {
     // TODO: only safe is output is at least 33 bit
     return sqrtf(SQUARE((int32_t)a->x - b->x) + SQUARE((int32_t)a->y - b->y));
 }
 
-uint32_t utils_distance_squared(const coordinates_t *a, const coordinates_t *b)
+inline float utils_distance_squared(const coordinates_t *a, const coordinates_t *b)
 {
     return SQUARE((int32_t)a->x - b->x) + SQUARE((int32_t)a->y - b->y);
 }
 
-uint32_t utils_distance_summed(const coordinates_t *a, const coordinates_t *b)
+inline float utils_distance_summed(const coordinates_t *a, const coordinates_t *b)
 {
     return abs((int32_t)a->x - b->x) + abs((int32_t)a->y - b->y);
 }
 
-uint32_t utils_distance_aproximated(const coordinates_t *a, const coordinates_t *b)
+inline float utils_distance_aproximated(const coordinates_t *a, const coordinates_t *b)
 {
     // https://www.flipcode.com/archives/Fast_Approximate_Distance_Functions.shtml
     // https://gamedev.stackexchange.com/questions/69241/how-to-optimize-the-distance-function
     uint32_t dx = abs(a->x - b->x);
     uint32_t dy = abs(a->y - b->y);
-    return (dx + dy + utils_max(dx, dy)) >> 1;
+    return (dx + dy + MAX(dx, dy)) >> 1;
 }
 
-uint32_t utils_get_rand32()
+inline uint32_t utils_get_rand32()
 {
 #ifdef CONFIG_KERNEL_BIN_NAME
     return sys_rand32_get();
@@ -66,18 +45,3 @@ uint32_t utils_get_rand32()
     return rand();
 #endif
 }
-
-// TODO
-// Move into #define MAX(a, b) a < b ? b : a for untyped + inlined version
-uint32_t utils_max(uint32_t a, uint32_t b)
-{
-    if (a >= b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
-
